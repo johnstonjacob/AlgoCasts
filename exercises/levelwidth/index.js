7ea3d1794833ff6a1dbe5e4e4f99cf3b7a4e95b3
jacob johnston <jacob@localhost.localdomain>
2018-04-10 08:05:45 -0500

    Add levelwidth

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,22 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+	var levels = [0 ]
+	var arr = [root, 's']
+
+	while ( arr.length > 1){
+		var node = arr.shift()
+
+		if ( node === 's' ){
+			arr.push(node)
+			levels.push(0)
+		}else{
+			levels[levels.length - 1]++
+			arr.push(...node.children)
+		}
+	}
+	return levels
+}
 
 module.exports = levelWidth;
